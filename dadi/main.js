e81a472f4510eaacaaea1754db19a8a1efc3b261
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-21 16:34:04 +0200

    forse finito esercizio dadi

--- dadi/main.js
+++ dadi/main.js
@@ -1,17 +1,24 @@
 //"Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
 var number = prompt('Inserisci un numero tra 1 e 6');
 var randomvariable = Math.floor(Math.random() * 6);
-console.log(randomvariable);
+document.writeln(randomvariable);
 
 if (isNaN(number)) {
-    alert('Non hai inserito un numero')
-} else {
-    if (number > randomvariable) {
-        alert('Hai vinto');
+    alert('Non hai inserito un numero');
 
-    } else if (number < randomvariable) {
-        alert('Hai perso');
+} else {
+    if (number < 0 || number > 6) {
+        alert('Devi inserire un numetro tra 1 e 6');
     } else {
-        alert('Pareggio');
+        if (number > randomvariable) {
+            alert('Hai vinto');
+
+        } else if (number < randomvariable) {
+            alert('Hai perso');
+        } else if (number = randomvariable) {
+            alert('Pareggio');
+        }
+
     }
+
 }
