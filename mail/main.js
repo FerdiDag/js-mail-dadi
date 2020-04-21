75a2c9bfc86009e890f63ff8cd47c2e3c141e9cd
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-21 15:33:21 +0200

    fine esercizio mail

--- mail/main.js
+++ mail/main.js
@@ -1,15 +1,24 @@
 //"Sono in lista?" Chiedi all'utente la sua email controlla che sia nella lista di chi può accedere (ergo: la mail è presente in un array contenente le email "autorizzate") e stampa un messaggio appropriato (sei in lista/non sei in lista)
 
 var mail = prompt('Qual è il tuo indirizzo email?');
-
-var listmail = ['ferdi.dagostino@gmail.com', 'pippofranco@gmail.com', 'peppe@gmail.com', 'mario@gmail.com','dario@gmail.com' ];
-
-
-
+var listmail = ['ferdi.dagostino@gmail.com', 'pippofranco@gmail.com', 'peppe@gmail.com', 'mario@gmail.com', 'dario@gmail.com'];
 if (mail.indexOf("@") === -1 || mail.indexOf(".") === -1) {
-    alert("Your email address must include one @ sign and one period");
-} else if (mail ==  listmail[0] ) {
-    alert('Sei in lista');
+    alert('Deve contenere una @ o un .')
 } else {
-    alert('Non sei in lista');
+    for (var i = 0; i < listmail.length; i++) {
+
+        if (mail == listmail[i]) {
+            alert('Sei in lista');
+        } 
+    }
 }
+
+
+
+
+//
+// } else if (mail == listmail[i]) {
+//     alert('Sei in lista');
+// } else {
+//     alert('Non sei in lista');
+// }
